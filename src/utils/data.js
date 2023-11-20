f31db6c1c7c9a1ada825623cc04af3e6b1a116f3
Wildan Yazid Ziddan <wildanziddan28@gmail.com>
2023-11-21 05:02:23 +0700

    first commit

---
+++ src/utils/data.js
@@ -0,0 +1,24 @@
+const getData = () => {
+    return [
+      {
+        id: 1,
+        name: 'Laurentia Deas Miracle Sinambela',
+        tag: '_poppicee',
+        imageUrl: '/images/yas.jpg',
+      },
+      {
+        id: 2,
+        name: 'Fahriel Aziz Satrio',
+        tag: 'seyyshin',
+        imageUrl: '/images/riel.jpg',
+      },
+      {
+        id: 3,
+        name: 'Muhammad Yan Nastihar',
+        tag: 'ivorycrz',
+        imageUrl: '/images/yan.jpg',
+      },
+    ];
+   }
+    
+   export { getData };
